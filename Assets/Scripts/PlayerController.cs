c52b3b07f7d34a9448f0110921ba711ec6b86567
RoughRaptors <RoughRaptors@Gmail.com>
2021-08-04 21:26:08 -0700

    Added a limiter between player movement input acceptance

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,63 +9,73 @@ namespace TEMPJAMNAMEREPLACEME
         public TileOccupier player;
         private Coroutine lerpCoroutine = null;
 
+        // for now, limit how fast the player can move to avoid any problems like moving diagonally or diagonal lerping
+        float lastMovementTime;
+        const float TIME_BETWEEN_MOVES = 0.25f;
+
         void Update()
         {
             LoadedLevel curLevel = GameManager.Instance.GetCurLevel();
             if (!(curLevel is null))
             {
-                bool isValidTile = false;
-                int newRow = 0;
-                int newCol = 0;
-                if (Input.GetKeyDown(KeyCode.D))
+                if (Time.time - lastMovementTime >= TIME_BETWEEN_MOVES)
                 {
-                    // moving right
-                    newRow = player.GetCurTile().GetRow();
-                    newCol = player.GetCurTile().GetCol() + 1;
-
-                    isValidTile = IsValidTile(newRow, newCol);
-                }
-                else if(Input.GetKeyDown(KeyCode.A))
-                {
-                    // moving left
-                    newRow = player.GetCurTile().GetRow();
-                    newCol = player.GetCurTile().GetCol() - 1;
-
-                    isValidTile = IsValidTile(newRow, newCol);
-
-                }
-                else if(Input.GetKeyDown(KeyCode.W))
-                {
-                    // moving up
-                    newRow = player.GetCurTile().GetRow() - 1;
-                    newCol = player.GetCurTile().GetCol();
-
-                    isValidTile = IsValidTile(newRow, newCol);
-                }
-                else if(Input.GetKeyDown(KeyCode.S))
-                {
-                    // moving down
-                    newRow = player.GetCurTile().GetRow() + 1;
-                    newCol = player.GetCurTile().GetCol();
-
-                    isValidTile = IsValidTile(newRow, newCol);
-                }
-
-                if (isValidTile)
-                {
-                    Tile newTile = GameManager.Instance.GetTileAtLocation(newRow, newCol);
-                    Vector3 newPos = new Vector3(newTile.GetPhysicalXPos(), newTile.GetPhysicalYPos());
-
-                    newTile.SetTileOccupier(curLevel.GetPlayer());
-                    GameManager.Instance.GetOccupierAtLocation(newRow, newCol).SetCurTile(newTile);
-
-                    // stop our old lerp if it exists, and lerp to our new position
-                    if (lerpCoroutine != null)
+                    bool isValidTile = false;
+                    int newRow = 0;
+                    int newCol = 0;
+                    if (Input.GetKeyDown(KeyCode.D))
                     {
-                        StopCoroutine(lerpCoroutine);
+                        // moving right
+                        newRow = player.GetCurTile().GetRow();
+                        newCol = player.GetCurTile().GetCol() + 1;
+
+                        isValidTile = IsValidTile(newRow, newCol);
+                    }
+                    else if (Input.GetKeyDown(KeyCode.A))
+                    {
+                        // moving left
+                        newRow = player.GetCurTile().GetRow();
+                        newCol = player.GetCurTile().GetCol() - 1;
+
+                        isValidTile = IsValidTile(newRow, newCol);
+
+                    }
+                    else if (Input.GetKeyDown(KeyCode.W))
+                    {
+                        // moving up
+                        newRow = player.GetCurTile().GetRow() - 1;
+                        newCol = player.GetCurTile().GetCol();
+
+                        isValidTile = IsValidTile(newRow, newCol);
+                    }
+                    else if (Input.GetKeyDown(KeyCode.S))
+                    {
+                        // moving down
+                        newRow = player.GetCurTile().GetRow() + 1;
+                        newCol = player.GetCurTile().GetCol();
+
+                        isValidTile = IsValidTile(newRow, newCol);
                     }
 
-                    lerpCoroutine = StartCoroutine(HandleMovePlayer(newPos));
+                    if (isValidTile)
+                    {
+                        Tile newTile = GameManager.Instance.GetTileAtLocation(newRow, newCol);
+                        Vector3 newPos = new Vector3(newTile.GetPhysicalXPos(), newTile.GetPhysicalYPos());
+
+                        newTile.SetTileOccupier(curLevel.GetPlayer());
+                        GameManager.Instance.GetOccupierAtLocation(newRow, newCol).SetCurTile(newTile);
+
+                        // stop our old lerp if it exists, and lerp to our new position
+                        if (lerpCoroutine != null)
+                        {
+                            StopCoroutine(lerpCoroutine);
+                        }
+
+                        lerpCoroutine = StartCoroutine(HandleMovePlayer(newPos));
+
+                        // keep track of our movement time
+                        lastMovementTime = Time.time;
+                    }
                 }
             }
         }
